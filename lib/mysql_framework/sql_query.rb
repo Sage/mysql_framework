dd6be4c60087d0aee371c5109254470c548807a2
Rob Kilby <rob.kilby@sage.com>
2018-08-09 22:13:46 +0100

    Adds support for new SQL clauses.
    Added increment and decrement methods that can be used with or without
    an existing set clause. These methods are intended to be used to
    increment or decrement an integer column entirely on the server.
    
    Also added support for offset and having clauses and tidied up the
    generated SQL a little.
    
    Signed-off-by: Rob Kilby <rob.kilby@sage.com>

--- lib/mysql_framework/sql_query.rb
+++ lib/mysql_framework/sql_query.rb
@@ -18,119 +18,177 @@ module MysqlFramework
 
     # This method is called to start a select query
     def select(*columns)
-      @sql = "select #{columns.join(',')}"
+      @sql = "SELECT #{columns.join(', ')}"
+
       self
     end
 
     # This method is called to start a delete query
     def delete
-      @sql = 'delete'
+      @sql = 'DELETE'
+
       self
     end
 
     # This method is called to start an update query
     def update(table, partition = nil)
-      @sql = "update #{table}"
-      @sql += " partition(p#{partition})" unless partition.nil?
+      @sql = "UPDATE #{table}"
+      @sql += " PARTITION (p#{partition})" unless partition.nil?
+
       self
     end
 
     # This method is called to start an insert query
     def insert(table, partition = nil)
-      @sql += "insert into #{table}"
-      @sql += " partition(p#{partition})" unless partition.nil?
+      @sql += "INSERT INTO #{table}"
+      @sql += " PARTITION (p#{partition})" unless partition.nil?
+
       self
     end
 
     # This method is called to specify the columns to insert into.
     def into(*columns)
-      @sql += " (#{columns.join(',')})"
+      @sql += " (#{columns.join(', ')})"
+
       self
     end
 
     # This method is called to specify the values to insert.
     def values(*values)
-      @sql += " values (#{values.map { |_v| '?' }.join(',')})"
-      values.each do |v|
-        @params << v
-      end
+      @sql += " VALUES (#{values.map { '?' }.join(', ')})"
+
+      values.each { |value| @params << value }
+
       self
     end
 
     # This method is called to specify the columns to update.
     def set(values)
-      @sql += ' set '
-      values.each do |k, p|
-        @sql += "`#{k}` = ?, "
-        @params << p
+      @sql += ' SET '
+
+      values.each do |key, param|
+        @sql += "`#{key}` = ?, "
+        @params << param
       end
-      @sql = @sql[0...-2]
+
+      @sql = @sql.chomp(', ')
+
+      self
+    end
+
+    def increment(values)
+      @sql += @sql.include?('SET') ? ', ' : ' SET '
+
+      values.each { |key, by| @sql += "`#{key}` = `#{key}` + #{by}, " }
+
+      @sql = @sql.chomp(', ')
+
+      self
+    end
+
+    def decrement(values)
+      @sql += @sql.include?('SET') ? ', ' : ' SET '
+
+      values.each { |key, by| @sql += "`#{key}` = `#{key}` - #{by}, " }
+
+      @sql = @sql.chomp(', ')
+
       self
     end
 
     # This method is called to specify the table/partition a select/delete query is for.
     def from(table, partition = nil)
-      @sql += " from #{table}"
-      @sql += " partition(p#{partition})" unless partition.nil?
+      @sql += " FROM #{table}"
+      @sql += " PARTITION (p#{partition})" unless partition.nil?
+
       self
     end
 
     # This method is called to specify a where clause for a query.
     def where(*conditions)
-      @sql += ' where' unless @sql.include?('where')
-      @sql += " (#{conditions.join(' and ')}) "
-      conditions.each do |c|
-        @params << c.value
-      end
+      @sql += ' WHERE' unless @sql.include?('WHERE')
+      @sql += " (#{conditions.join(' AND ')}) "
+
+      conditions.each { |condition| @params << condition.value }
+
       self
     end
 
     # This method is called to add an `and` keyword to a query to provide additional where clauses.
     def and
-      @sql += 'and'
+      @sql += 'AND'
+
       self
     end
 
     # This method is called to add an `or` keyword to a query to provide alternate where clauses.
     def or
-      @sql += 'or'
+      @sql += 'OR'
+
       self
     end
 
     # This method is called to add an `order by` statement to a query
     def order(*columns)
-      @sql += " order by #{columns.join(',')}"
+      @sql += " ORDER BY #{columns.join(', ')}"
+
       self
     end
 
     # This method is called to add an `order by ... desc` statement to a query
     def order_desc(*columns)
       order(*columns)
-      @sql += ' desc'
+
+      @sql += ' DESC'
+
       self
     end
 
     # This method is called to add a limit to a query
     def limit(count)
-      @sql += " limit #{count}"
+      @sql += " LIMIT #{count}"
+
+      self
+    end
+
+    # This method is called to add an offset to a query
+    def offset(offset)
+      raise 'A limit clause must be supplied to use an offset' unless @sql.include?('LIMIT')
+
+      @sql += " OFFSET #{offset}"
+
       self
     end
 
     # This method is called to add a join statement to a query.
-    def join(table)
-      @sql += " join #{table}"
+    def join(table, type: nil)
+      @sql += " #{type.upcase}" unless type.nil?
+      @sql += " JOIN #{table}"
+
       self
     end
 
     # This method is called to add the `on` detail to a join statement.
     def on(column_1, column_2)
-      @sql += " on #{column_1} = #{column_2}"
+      @sql += " ON #{column_1} = #{column_2}"
+
       self
     end
 
     # This method is called to add a `group by` statement to a query
     def group_by(*columns)
-      @sql += " group by #{columns.join(',')}"
+      @sql += " GROUP BY #{columns.join(', ')}"
+
+      self
+    end
+
+    # This method is called to specify a having clause for a query.
+    def having(*conditions)
+      @sql += ' HAVING' unless @sql.include?('HAVING')
+      @sql += " (#{conditions.join(' AND ')}) "
+
+      conditions.each { |condition| @params << condition.value }
+
       self
     end
   end
--- spec/lib/mysql_framework/sql_query_spec.rb
+++ spec/lib/mysql_framework/sql_query_spec.rb
@@ -20,7 +20,7 @@ describe MysqlFramework::SqlQuery do
           '2018-06-28 10:00:00'
         )
 
-      expect(subject.sql).to eq('insert into `gems` partition(p15) (`gems`.`name`,`gems`.`author`,`gems`.`created_at`,`gems`.`updated_at`) values (?,?,?,?)')
+      expect(subject.sql).to eq('INSERT INTO `gems` PARTITION (p15) (`gems`.`name`, `gems`.`author`, `gems`.`created_at`, `gems`.`updated_at`) VALUES (?, ?, ?, ?)')
       expect(subject.params).to eq(['mysql_framework', 'sage', '2018-06-28 10:00:00', '2018-06-28 10:00:00'])
     end
 
@@ -30,32 +30,23 @@ describe MysqlFramework::SqlQuery do
           name: 'mysql_framework',
           updated_at: '2018-06-28 13:00:00'
         )
-        .where(
-          gems[:id].eq('12345')
-        )
+        .where(gems[:id].eq('12345'))
 
-      expect(subject.sql).to eq('update `gems` partition(p20) set `name` = ?, `updated_at` = ? where (`gems`.`id` = ?)')
+      expect(subject.sql).to eq('UPDATE `gems` PARTITION (p20) SET `name` = ?, `updated_at` = ? WHERE (`gems`.`id` = ?)')
       expect(subject.params).to eq(['mysql_framework', '2018-06-28 13:00:00', '12345'])
     end
 
     it 'builds the delete query as expected' do
-      subject.delete.from(gems, 30)
-        .where(
-          gems[:id].eq('45678')
-        )
+      subject.delete.from(gems, 30).where(gems[:id].eq('45678'))
 
-      expect(subject.sql).to eq('delete from `gems` partition(p30) where (`gems`.`id` = ?)')
+      expect(subject.sql).to eq('DELETE FROM `gems` PARTITION (p30) WHERE (`gems`.`id` = ?)')
       expect(subject.params).to eq(['45678'])
     end
 
     it 'builds a basic select query as expected' do
-      subject.select('*')
-        .from(gems, 40)
-        .where(
-          gems[:id].eq('9876')
-        )
+      subject.select('*').from(gems, 40).where(gems[:id].eq('9876'))
 
-      expect(subject.sql).to eq('select * from `gems` partition(p40) where (`gems`.`id` = ?)')
+      expect(subject.sql).to eq('SELECT * FROM `gems` PARTITION (p40) WHERE (`gems`.`id` = ?)')
       expect(subject.params).to eq(['9876'])
     end
 
@@ -63,11 +54,9 @@ describe MysqlFramework::SqlQuery do
       subject.select('*')
         .from(gems, 40)
         .join(versions).on(versions[:gem_id], gems[:id])
-        .where(
-          gems[:id].eq('9876')
-        )
+        .where(gems[:id].eq('9876'))
 
-      expect(subject.sql).to eq('select * from `gems` partition(p40) join `versions` on `versions`.`gem_id` = `gems`.`id` where (`gems`.`id` = ?)')
+      expect(subject.sql).to eq('SELECT * FROM `gems` PARTITION (p40) JOIN `versions` ON `versions`.`gem_id` = `gems`.`id` WHERE (`gems`.`id` = ?)')
       expect(subject.params).to eq(['9876'])
     end
   end
@@ -75,27 +64,31 @@ describe MysqlFramework::SqlQuery do
   describe '#select' do
     it 'sets the sql for a select statement' do
       subject.select(gems[:id], gems[:name])
-      expect(subject.sql).to eq('select `gems`.`id`,`gems`.`name`')
+
+      expect(subject.sql).to eq('SELECT `gems`.`id`, `gems`.`name`')
     end
   end
 
   describe '#delete' do
     it 'sets the sql for a delete statement' do
       subject.delete
-      expect(subject.sql).to eq('delete')
+
+      expect(subject.sql).to eq('DELETE')
     end
   end
 
   describe '#update' do
     it 'sets the sql for an update statement' do
       subject.update(gems)
-      expect(subject.sql).to eq('update `gems`')
+
+      expect(subject.sql).to eq('UPDATE `gems`')
     end
 
     context 'when a partition is specified' do
       it 'sets the sql for an update statement' do
         subject.update(gems, 25)
-        expect(subject.sql).to eq('update `gems` partition(p25)')
+
+        expect(subject.sql).to eq('UPDATE `gems` PARTITION (p25)')
       end
     end
   end
@@ -103,13 +96,15 @@ describe MysqlFramework::SqlQuery do
   describe '#insert' do
     it 'sets the sql for an insert statement' do
       subject.insert(gems)
-      expect(subject.sql).to eq('insert into `gems`')
+
+      expect(subject.sql).to eq('INSERT INTO `gems`')
     end
 
     context 'when a partition is specified' do
       it 'sets the sql for an insert statement' do
         subject.insert(gems, 35)
-        expect(subject.sql).to eq('insert into `gems` partition(p35)')
+
+        expect(subject.sql).to eq('INSERT INTO `gems` PARTITION (p35)')
       end
     end
   end
@@ -117,55 +112,91 @@ describe MysqlFramework::SqlQuery do
   describe '#into' do
     it 'sets the sql for an into statement' do
       subject.into(gems[:name], gems[:author], gems[:created_at])
-      expect(subject.sql).to eq('(`gems`.`name`,`gems`.`author`,`gems`.`created_at`)')
+
+      expect(subject.sql).to eq('(`gems`.`name`, `gems`.`author`, `gems`.`created_at`)')
     end
   end
 
   describe '#values' do
     it 'sets the sql for the values statement' do
       subject.values('mysql_framework', 'sage', '2016-06-28 10:00:00')
-      expect(subject.sql).to eq('values (?,?,?)')
+
+      expect(subject.sql).to eq('VALUES (?, ?, ?)')
     end
   end
 
   describe '#set' do
     it 'sets the sql for the set statement' do
       subject.set(name: 'mysql_framework', author: 'sage', created_at: '2016-06-28 10:00:00')
-      expect(subject.sql).to eq('set `name` = ?, `author` = ?, `created_at` = ?')
+
+      expect(subject.sql).to eq('SET `name` = ?, `author` = ?, `created_at` = ?')
+    end
+  end
+
+  describe '#increment' do
+    it 'appends the sql for the increment statement' do
+      subject.set(updated_at: '2016-01-15 19:00:00').increment(count: 1)
+
+      expect(subject.sql).to eq('SET `updated_at` = ?, `count` = `count` + 1')
+    end
+
+    context 'when a set statement has not been issued' do
+      it 'appends the sql for the increment statement' do
+        subject.increment(count: 1)
+
+        expect(subject.sql).to eq('SET `count` = `count` + 1')
+      end
+    end
+  end
+
+  describe '#decrement' do
+    it 'appends the sql for the decrement statement' do
+      subject.set(updated_at: '2016-01-15 19:00:00').decrement(count: 1)
+
+      expect(subject.sql).to eq('SET `updated_at` = ?, `count` = `count` - 1')
+    end
+
+    context 'when a set statement has not been issued' do
+      it 'appends the sql for the decrement statement' do
+        subject.decrement(count: 1)
+
+        expect(subject.sql).to eq('SET `count` = `count` - 1')
+      end
     end
   end
 
   describe '#from' do
     it 'sets the sql for a from statement' do
       subject.from(gems)
-      expect(subject.sql).to eq('from `gems`')
+
+      expect(subject.sql).to eq('FROM `gems`')
     end
 
     context 'when a partition is specified' do
       it 'sets the sql for a from statement' do
         subject.from(gems, 45)
-        expect(subject.sql).to eq('from `gems` partition(p45)')
+
+        expect(subject.sql).to eq('FROM `gems` PARTITION (p45)')
       end
     end
   end
 
   describe '#where' do
-    before :each do
-      subject.where(gems['author'].eq('sage'), gems['created_at'].gt('2018-01-01 00:00:00'))
-    end
+    before(:each) { subject.where(gems[:author].eq('sage'), gems[:created_at].gt('2018-01-01 00:00:00')) }
 
     it 'appends where to the sql' do
-      expect(subject.sql).to include('where')
+      expect(subject.sql).to include('WHERE')
     end
 
     it 'sets the sql for the where statement' do
-      expect(subject.sql).to eq('where (`gems`.`author` = ? and `gems`.`created_at` > ?)')
+      expect(subject.sql).to eq('WHERE (`gems`.`author` = ? AND `gems`.`created_at` > ?)')
     end
 
     context 'when the sql already contains a where' do
       it 'does not append an extra where' do
-        subject.and.where(gems['name'].eq('mysql_framework'))
-        expect(subject.sql).to eq('where (`gems`.`author` = ? and `gems`.`created_at` > ?) and (`gems`.`name` = ?)')
+        subject.and.where(gems[:name].eq('mysql_framework'))
+
+        expect(subject.sql).to eq('WHERE (`gems`.`author` = ? AND `gems`.`created_at` > ?) AND (`gems`.`name` = ?)')
       end
     end
   end
@@ -173,56 +204,106 @@ describe MysqlFramework::SqlQuery do
   describe '#and' do
     it 'appends the sql for an and statement' do
       subject.and
-      expect(subject.sql).to eq('and')
+
+      expect(subject.sql).to eq('AND')
     end
   end
 
   describe '#or' do
     it 'appends the sql for an or statement' do
       subject.or
-      expect(subject.sql).to eq('or')
+
+      expect(subject.sql).to eq('OR')
     end
   end
 
   describe '#order' do
     it 'appends the sql for an order statement' do
       subject.order(gems[:created_at], gems[:updated_at])
-      expect(subject.sql).to eq('order by `gems`.`created_at`,`gems`.`updated_at`')
+
+      expect(subject.sql).to eq('ORDER BY `gems`.`created_at`, `gems`.`updated_at`')
     end
   end
 
   describe '#order_desc' do
     it 'appends the sql for an order descending statement' do
       subject.order_desc(gems[:created_at], gems[:updated_at])
-      expect(subject.sql).to eq('order by `gems`.`created_at`,`gems`.`updated_at` desc')
+
+      expect(subject.sql).to eq('ORDER BY `gems`.`created_at`, `gems`.`updated_at` DESC')
     end
   end
 
   describe '#limit' do
     it 'appends the sql for a limit statement' do
       subject.limit(10)
-      expect(subject.sql).to eq('limit 10')
+
+      expect(subject.sql).to eq('LIMIT 10')
+    end
+  end
+
+  describe '#offset' do
+    it 'appends the sql for an offset statement' do
+      subject.limit(10).offset(5)
+
+      expect(subject.sql).to eq('LIMIT 10 OFFSET 5')
+    end
+
+    context 'when a limit statement is not found' do
+      it 'raises an error' do
+        expect { subject.offset(5) }.to raise_error(RuntimeError)
+      end
     end
   end
 
   describe '#join' do
     it 'appends the sql for a join statement' do
       subject.join(versions)
-      expect(subject.sql).to eq('join `versions`')
+
+      expect(subject.sql).to eq('JOIN `versions`')
+    end
+
+    context 'when a type is supplied' do
+      it 'appends the sql for a join statement' do
+        subject.join(versions, type: 'LEFT OUTER')
+
+        expect(subject.sql).to eq('LEFT OUTER JOIN `versions`')
+      end
     end
   end
 
   describe '#on' do
     it 'appends the sql for the on statement' do
       subject.on(gems[:id], versions[:gem_id])
-      expect(subject.sql).to eq('on `gems`.`id` = `versions`.`gem_id`')
+
+      expect(subject.sql).to eq('ON `gems`.`id` = `versions`.`gem_id`')
     end
   end
 
   describe '#group_by' do
     it 'appends the sql for a group by statement' do
       subject.group_by(gems[:created_at], gems[:updated_at])
-      expect(subject.sql).to eq('group by `gems`.`created_at`,`gems`.`updated_at`')
+
+      expect(subject.sql).to eq('GROUP BY `gems`.`created_at`, `gems`.`updated_at`')
+    end
+  end
+
+  describe '#having' do
+    before(:each) { subject.having(gems[:count].gt(1), gems[:count].lt(10)) }
+
+    it 'appends having to the sql' do
+      expect(subject.sql).to include('HAVING')
+    end
+
+    it 'sets the sql for the having statement' do
+      expect(subject.sql).to eq('HAVING (`gems`.`count` > ? AND `gems`.`count` < ?)')
+    end
+
+    context 'when the sql already contains a having' do
+      it 'does not append an extra having' do
+        subject.or.having(gems[:count].gt(20))
+
+        expect(subject.sql).to eq('HAVING (`gems`.`count` > ? AND `gems`.`count` < ?) OR (`gems`.`count` > ?)')
+      end
     end
   end
 end
