eb163a362ce85d4e91d40b63e54ee4750a7f276b
Ian Oxley <ian.oxley@sage.com>
2020-06-18 14:47:58 +0100

    Add support for IS NULL / IS NOT NULL
    Update `SqlCondition` to add support for queries with where clauses such
    as `column IS NULL` or `column IS NOT NULL`.
    
    If `comparison` is set to either `IS NULL` or `IS NOT NULL` then you
    shouldn't pass a `value` to the constructor. If you do then an
    `ArgumentError` is raised.

--- lib/mysql_framework/sql_condition.rb
+++ lib/mysql_framework/sql_condition.rb
@@ -3,18 +3,31 @@
 module MysqlFramework
 # This class is used to represent a Sql Condition for a column.
   class SqlCondition
+    NIL_COMPARISONS = ['IS NULL', 'IS NOT NULL'].freeze
+
     # This method is called to get the value of this condition for prepared statements.
     attr_reader :value
 
-    def initialize(column:, comparison:, value:)
+    def initialize(column:, comparison:, value: nil)
       @column = column
       @comparison = comparison
+
+      raise ArgumentError, "Cannot set value when comparison is #{comparison}" if nil_comparison? && value != nil
+
       @value = value
     end
 
     # This method is called to get the condition as a string for a sql prepared statement
     def to_s
+      return "#{@column} #{@comparison.upcase}" if nil_comparison?
+
       "#{@column} #{@comparison} ?"
+    end
+
+    private
+
+    def nil_comparison?
+      NIL_COMPARISONS.include?(@comparison.upcase)
     end
   end
 end
--- spec/lib/mysql_framework/sql_condition_spec.rb
+++ spec/lib/mysql_framework/sql_condition_spec.rb
@@ -8,4 +8,72 @@ describe MysqlFramework::SqlCondition do
       expect(subject.to_s).to eq('version = ?')
     end
   end
+
+  context 'when comparison is IS NULL' do
+    subject { described_class.new(column: 'version', comparison: 'IS NULL') }
+
+    it 'has a nil value by default' do
+      expect(subject.value).to be_nil
+    end
+
+    context 'when a value is passed to the constructor' do
+      subject { described_class.new(column: 'version', comparison: 'IS NULL', value: 'foo') }
+
+      describe '#new' do
+        it 'raises an ArgumentError if value is set' do
+          expect { subject }.to raise_error(ArgumentError, 'Cannot set value when comparison is IS NULL')
+        end
+      end
+    end
+
+    describe '#to_s' do
+      it 'does not include a value placeholder' do
+        expect(subject.to_s).to eq('version IS NULL')
+      end
+    end
+  end
+
+  context 'when comparison is lowercase is null' do
+    subject { described_class.new(column: 'version', comparison: 'is null') }
+
+    describe '#to_s' do
+      it 'ignores case' do
+        expect(subject.to_s).to eq 'version IS NULL'
+      end
+    end
+  end
+
+  context 'when comparison is IS NOT NULL' do
+    subject { described_class.new(column: 'version', comparison: 'IS NOT NULL') }
+
+    it 'has a nil value by default' do
+      expect(subject.value).to be_nil
+    end
+
+    context 'when a value is passed to the constructor' do
+      subject { described_class.new(column: 'version', comparison: 'IS NOT NULL', value: 'foo') }
+
+      describe '#new' do
+        it 'raises an ArgumentError if value is set' do
+          expect { subject }.to raise_error(ArgumentError, 'Cannot set value when comparison is IS NOT NULL')
+        end
+      end
+    end
+
+    describe '#to_s' do
+      it 'does not include a value placeholder' do
+        expect(subject.to_s).to eq('version IS NOT NULL')
+      end
+    end
+  end
+
+  context 'when comparison is lowercase is not null' do
+    subject { described_class.new(column: 'version', comparison: 'is not null') }
+
+    describe '#to_s' do
+      it 'ignores case' do
+        expect(subject.to_s).to eq 'version IS NOT NULL'
+      end
+    end
+  end
 end
